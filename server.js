f92a0972d05c06b370731b87343ef46ff550ac4b
VedantA2006 <202401110006@mitaoe.ac.in>
2026-03-14 00:03:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ const authRoute = require("./routes/auth")
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: "*",
+  methods: ["GET","POST"],
+  allowedHeaders: ["Content-Type"]
+}))
 app.use(express.json())
 app.use(express.static("public"))
 
